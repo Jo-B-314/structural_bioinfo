1d007b0b2c4d79aca7ef47c1ff2cbcf6f0863f2e
talhar123456 <48639541+talhar123456@users.noreply.github.com>
2021-12-02 13:43:24 +0100

    task 4 incomplete

---
+++ assignment03/rehman_becher_task4_assignment3.py
@@ -0,0 +1,72 @@
+import re
+import pandas as pd
+import numpy
+# from Bio import AlignIO
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import logomaker as lm
+
+
+# align = AlignIO.read("Clustalw/opuntia.aln", "clustal")
+# print(align)
+
+# file = open('seqMsa.fasta', 'r')
+file = open('sample.txt', 'r')
+# file = open('newSampleMSA.txt', 'r')
+
+fasta = file.read()
+fastaList = fasta.split(' ')
+# print(fastaList)
+str1 = ','.join(fastaList)
+# print(str1)
+fastaList = str1.split('\n')
+# print(fastaList)
+str1 = '\t'.join(fastaList)
+# print(str1)
+fastaList = str1.split(',')
+# print(fastaList)
+str1 = ' '.join(fastaList)
+# print(str1)
+fastaList = str1.split('\t')
+# print(fastaList)
+
+res = []
+id = []
+count = 0
+
+for i in fastaList:
+    if re.findall('.*_.*', i):
+        count += 1
+        res.append(i[12:])
+        id.append(i[:12])
+
+dict = {i: id.count(i) for i in id}
+totalRes = len(dict.keys())
+
+# print('total unique ids***************', totalRes)
+# print(id)
+# print('res==', res)
+# compare = res
+# print('compare==', compare)
+
+# inserting charecter in seq
+# for i in range(0, len(res) + 3, totalRes + 1):
+#     res.insert(i, '!')
+    # print(i)
+
+for i in res:
+    print(i)
+
+counts_mat = lm.alignment_to_matrix(res)
+print(counts_mat.head())
+lm.Logo(counts_mat)
+
+# column = []
+# for i in res:
+#     # if i != '!':
+#     for x in range(0, len(i), len(i)):
+#         column.append(i[x])
+#         # print(x)
+# print(column)
